89547126fe3d65a43337041b18422c2fb7482392
Adam <adamgabriel19922@gmail.com>
2026-04-29 13:05:31 -0700

    Expose kernel contracts

---
+++ opt/ark/runtime/kernel/__init__.py
@@ -0,0 +1,7 @@
+from .checkpoints import Checkpoint
+from .cube import CubeFace, CubeState
+from .envelope import OperationEnvelope
+from .lifecycle import LifecycleRecord, LifecycleState
+from .oda import ODAFrame
+
+__all__=['Checkpoint','CubeFace','CubeState','LifecycleRecord','LifecycleState','ODAFrame','OperationEnvelope']
